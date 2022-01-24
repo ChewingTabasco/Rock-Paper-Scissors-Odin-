26284a22a8039e67ed8b6e9af40f336d9183b120
ChewingTabasco <emerickjustice@gmail.com>
2022-01-24 18:56:56 -0500

    adding game logic

--- script.js
+++ script.js
@@ -4,13 +4,17 @@
 // log the return to the console
 
 let randomNumber;
+let playerWin = false;
+let computerWin = false;
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
   getRandomNumber();
   if (randomNumber == 0) {
-    return "Rock!";
+    return "Rock";
   } else if (randomNumber == 1) {
-    return "Paper!";
+    return "Paper";
   } else if (randomNumber == 2) {
     return "Scissors";
   }
@@ -20,4 +24,59 @@ function getRandomNumber() {
   randomNumber = Math.floor(Math.random() * 3);
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toUpperCase() == "ROCK") {
+    if (computerSelection == "Paper") {
+      incrementScore(computerScore);
+      return `You lose. The computer played ${computerSelection}.`;
+    } else if (computerSelection == "Scissors") {
+      incrementScore(playerScore);
+      return `You win! The computer played ${computerSelection}!`;
+    } else {
+      return `Tie game. You both played ${playerSelection}.`;
+    }
+  }
+  if (playerSelection.toUpperCase() == "PAPER") {
+    if (computerSelection == "Scissors") {
+      incrementScore(computerScore);
+      return `You lose. The computer played ${computerSelection}.`;
+    } else if (computerSelection == "Rock") {
+      incrementScore(playerScore);
+      return `You win! The computer played ${computerSelection}!`;
+    } else {
+      return `Tie game. You both played ${playerSelection}.`;
+    }
+  }
+  if (playerSelection.toUpperCase() == "SCISSORS") {
+    if (computerSelection == "Rock") {
+      incrementScore(computerScore);
+      return `You lose. The computer played ${computerSelection}.`;
+    } else if (computerSelection == "Paper") {
+      incrementScore(playerScore);
+      return `You win! The computer played ${computerSelection}!`;
+    } else {
+      return `Tie game. You both played ${playerSelection}.`;
+    }
+  } else {
+    return `That isn't how you play the game...`;
+  }
+}
+
+function incrementScore(player) {
+  player += 1;
+}
+
+function game() {
+  console.log("You play 5 rounds of Rock Paper Scissors. The results are...");
+  for (let i = 1; i < 6; i++) {
+    console.log(playRound("Rock", computerPlay()));
+  }
+}
+
+game();
+console.log(`Your Score: ${playerScore}
+ Computer Score: ${computerScore}`);
+
+//for each time the player/ computer wins, xScore += 1. run a function that prints the total scores for each. run another function that declares the winner with a string.
+
+// console.log(playRound("Rock", computerPlay()));
